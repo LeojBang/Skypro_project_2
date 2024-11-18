6b099168008dace7e1f729ff999462f07774555d
maksim <m.merkulovdodo@gmail.com>
2024-11-18 23:16:54 +0300

    Homework 14_1 is done

---
+++ tests/test_product.py
@@ -0,0 +1,5 @@
+def test_product(products):
+    assert products.name == "Samsung Galaxy C23 Ultra"
+    assert products.description == "256GB, Серый цвет, 200MP камера"
+    assert products.price == 180000.0
+    assert products.quantity == 5
